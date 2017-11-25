d7d174f9f28124058612009b068750107bb1110d
Antonios Klimis <antonios.klimis@gmail.com>
2017-11-25 22:02:23 +0200

    fixed point

--- app/views/Organizer.js
+++ app/views/Organizer.js
@@ -91,7 +91,7 @@ class Organizer extends Component {
                                 <div id="tab-1" className="tab-pane active">
                                     <div className="panel-body">
                                         <div className="text-right">
-                                            <h1 className={classNameOverBudget}>{`${this.state.meetinghr} hr = €${cost.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')} `}</h1>
+                                            <h1 className={classNameOverBudget}>{`${this.state.meetinghr.toFixed(1)} hr = €${cost.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')} `}</h1>
                                             <span className={classNameOverBudget}>{overBudget ? '* total budget restriction rule violated' : ''}</span>
                                         </div>
                                         <fieldset className="form-horizontal">
