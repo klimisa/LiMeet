9ddafcfdb652c6f4d452e8260dd7e59389d1afce
Antonios Klimis <antonios.klimis@gmail.com>
2017-11-26 10:22:16 +0200

    rules

--- app/views/Rules.js
+++ app/views/Rules.js
@@ -7,13 +7,23 @@ class Rules extends Component {
             <div className="wrapper wrapper-content animated fadeInRight">
                 <div className="row">
                     <div className="col-lg-12">
-                        <div className="text-center m-t-lg">
-                            <h1>
-                                Sample example of second view
-                            </h1>
-                            <small>
-                                Written in Rules.js component
-                            </small>
+                        <div className="ibox">
+                            <div className="ibox-content">
+                                <div className="no-padding">
+                                    <h2>General Rules</h2>
+                                    <ul className="list-group">
+                                        <li className="list-group-item">Absolute Meeting Bufget: €1.000</li>
+                                        <li className="list-group-item">No more than 50% of available for anyone</li>
+                                        <li className="list-group-item">No more than 10% of available time for Devs</li>
+                                    </ul>
+                                    <h2>Budgetary Rules</h2>
+                                    <ul className="list-group">
+                                        <li className="list-group-item">While in Design, budget for Chile no more than €20.000</li>
+                                        <li className="list-group-item">While in Dev, budget for Chile no more than €5.000</li>
+                                        <li className="list-group-item">At kick-off, all budget for Chile is suspended</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
